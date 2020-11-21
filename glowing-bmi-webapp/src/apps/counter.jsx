b6fb8dc714d1d7fbcca9b9fa14330d3724ba2278
Pablo Fernández Franco <pableudo@gmail.com>
2020-11-21 18:23:58 +0000

    added classes part

--- glowing-bmi-webapp/src/apps/counter.jsx
+++ glowing-bmi-webapp/src/apps/counter.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import InputCounter from '../components/InputCounter';
+import Storage from '../lib/storage';
+
+const storage = Storage('state');
 
 class CounterApp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            weight: 60
+            weight: 60,
         };
+
+        // Problem solved
+        // this.state = storage.get() || { weight: 60 };
 
         this.increment = this.increment.bind(this);
         this.decrement = this.decrement.bind(this);
@@ -48,6 +54,18 @@ class CounterApp extends React.Component {
         // };
 
         // this.setState(incrementState);
+
+        // v7
+        // this.setState({ weight: this.state.weight + 1}, () => console.log('After', this.state));
+        // console.log('Before', this.state);
+
+        // v8
+        // this.setState({ weight: this.state.weight + 1 }, () => {
+        //     document.title = `Your weight is ${this.state.weight}`;
+        // });
+
+        // Problem solved
+        // this.setState({ weight: this.state.weight + 1 }, () => storage.save(this.state));
         
     }
 
